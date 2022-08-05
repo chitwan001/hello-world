ad1d49ca910bda78c5229633508b14c1e3b80d6c
Chitwan Bindal <chitwan001@gmail.com>
2022-08-05 09:52:06 +0530

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<10;i++){
+        cout<<"Hello World \n";
+    }
+    //It prints hello world 10 times
+    return 0;
+}
